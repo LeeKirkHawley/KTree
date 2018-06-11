d93937f230c4acb5d5bf911a567238713572417b
Kirk Hawley <leekirkhawley@gmail.com>
2018-06-11 07:23:08 -0700

    delete node, start trying to display list

--- KTree/KBTree.h
+++ KTree/KBTree.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <list>
 #include <memory>
+#include <vector>
+#include <assert.h>
 
 using namespace std;
 
@@ -11,34 +13,34 @@ using namespace std;
 template<class T> class KBTree
 {
 private:
-	KBTreeNode<T>* FindNode(unique_ptr<KBTreeNode<T>>& Node, T value)
+	unique_ptr<KBTreeNode<T>>& FindNode(unique_ptr<KBTreeNode<T>>& Parent, T value)
 	{
-		if (Node->_value == value)
-			return Node.get();
+		if (Parent->_value == value)
+			return Parent;
 
-		if (Node->_value > value)
+		if (Parent->_value > value)
 		{
-			if (Node->_leftChild != NULL)
-				return FindNode(Node->_leftChild, value);
+			if (Parent->_leftChild != NULL)
+				return FindNode(Parent->_leftChild, value);
 			else
-				return NULL;
+				throw;
 		}
 		else
 		{
-			if (Node->_rightChild != NULL)
-				return FindNode(Node->_rightChild, value);
+			if (Parent->_rightChild != NULL)
+				return FindNode(Parent->_rightChild, value);
 			else
-				return NULL;
+				throw;
 		}
 	}
 
 
 public:
-	unique_ptr<KBTreeNode<T>> _head;
+	unique_ptr<KBTreeNode<T>> _root;
 
 	KBTree<T>()
 	{
-		_head = NULL;
+		_root = NULL;
 	};
 	~KBTree<T>()
 	{
@@ -46,21 +48,102 @@ public:
 
 	void Add(T value)
 	{
-		if (_head == NULL)
+		if (_root == NULL)
 		{
-			_head = make_unique<KBTreeNode<T>>(value);
+			_root = make_unique<KBTreeNode<T>>(value);
 		}
 		else
 		{
-			_head->Add(value);
+			_root->Add(value);
 		}
 	}
 	
-	KBTreeNode<T>* FindNode(T value)
+	unique_ptr<KBTreeNode<T>>& FindNode(T value)
 	{
-		return FindNode(_head, value);
+		return FindNode(_root, value);
 	}
 
+	void RemoveNode(unique_ptr<KBTreeNode<T>>& Parent, T value)
+	{
+		if (Parent->_leftChild->_value == value)
+		{
+			unique_ptr<KBTreeNode<T>>& NodeToDelete = Parent->_leftChild;
+			if (NodeToDelete->_leftChild == nullptr && NodeToDelete->_rightChild == nullptr)  // it's a leaf
+			{
+				Parent->_leftChild = nullptr;
+				NodeToDelete->reset(nulltpr);
+			}
+			return;
+		}
 
+		if (Parent->_rightChild->_value == value)
+		{
+			unique_ptr<KBTreeNode<T>>& NodeToDelete = Parent->_rightChild;
+			if (NodeToDelete->_leftChild == nullptr && NodeToDelete->_rightChild == nullptr)  // it's a leaf
+			{
+				Parent->_rightChild = nullptr;
+				NodeToDelete->reset();
+			}
+			return;
+		}
+
+		//if (Parent->_value > value)
+		//{
+		//	if (Parent->_leftChild != NULL)
+		//		return FindNode(Parent->_leftChild, value);
+		//	else
+		//		throw;
+		//}
+		//else
+		//{
+		//	if (Parent->_rightChild != NULL)
+		//		return FindNode(Parent->_rightChild, value);
+		//	else
+		//		throw;
+		//}
+
+	}
+
+	void RemoveNode(T value)
+	{
+		if (_root->_value == value)   // don't know parent, might be root
+			throw;
+
+		RemoveNode(_root, value);
+	}
+
+	vector<string> BuildLevelList()
+	{
+		vector<string> Levels;
+
+		BuildLevelString(Levels, _root, 0);
+
+		return Levels;
+	}
+
+		void BuildLevelString(vector<string>& Levels, unique_ptr<KBTreeNode<T>>& Parent, int Level)
+	{
+		assert(Level >= 0);
+
+		int ListHeight = Levels.size();
+		if (ListHeight <= Level)
+			Levels.push_back("");
+
+		int LineLength = 20;
+		LineLength /= (Level + 1);  // no zeroes eh!
+
+		for(int i = 0;i < LineLength;i++)
+			Levels[Level].append("-");
+
+		Levels[Level].append(Parent->_value);
+		for (int i = 0; i < LineLength; i++)
+			Levels[Level].append("-");
+
+		if (Parent->_leftChild)
+			BuildLevelString(Levels, Parent->_leftChild, Level + 1);
+
+		if (Parent->_rightChild)
+			BuildLevelString(Levels, Parent->_rightChild, Level + 1);
+	}
 };
 
